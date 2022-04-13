4c0848d7e4666526653eb42983c640e8c1173f7c
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-04-13 15:18:24 +0800

    test

--- MacMusicPlayer/ViewModels/MainViewModel.cs
+++ MacMusicPlayer/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ namespace MacMusicPlayer.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        
         public string Greeting => "Welcome to Avalonia!";
 
         public MainViewModel()
